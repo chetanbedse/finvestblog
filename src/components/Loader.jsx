66aef28adc7479a24b59ca48a1a4ff215df048f5
chetanbedse <chetanbedse5041@gmail.com>
2024-01-25 22:58:55 +0530

    commit

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { BallTriangle } from "react-loader-spinner";
 
@@ -18,24 +17,3 @@ function Loader() {
 }
 
 export default Loader;
-=======
-import React from "react";
-import { BallTriangle } from "react-loader-spinner";
-
-function Loader() {
-  return (
-    <BallTriangle
-      height={100}
-      width={100}
-      radius={5}
-      color="#b75660"
-      ariaLabel="ball-triangle-loading"
-      wrapperStyle={{}}
-      wrapperClass=""
-      visible={true}
-    />
-  );
-}
-
-export default Loader;
->>>>>>> cba93fa114fddd17369af3215104a4a2b6a2bdab
